c4ec37fd3fc05472e68c01ec53ef13e3bf07e5dc
HOJONG_LAPTOP <jeontha@gmail.com>
2018-06-27 04:28:18 +0900

    Changed css

--- routes/mypage.js
+++ routes/mypage.js
@@ -1,17 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.route('/')
-.post((req, res)=>{
-
-    var sql, inserts;
-
-    sql1 = 'select * from Track where Track_id = (select F_track from Student where student_id=?)';
-    sql2 = 'select * from Track where Track_id = (select S_track from Student where student_id=?)';
-    console.log(sql1);
-    inserts = [req.body.student_id];
-    console.log(inserts);
+.get((req, res)=>{
+    var sql1 = 'select * from Track where Track_id = (select F_track from Student where student_id=?)';
+    var sql2 = 'select * from Track where Track_id = (select S_track from Student where student_id=?)';
+    var inserts = [req.session.student_id];
     pool.query( sql1, inserts , function( err, rows1 ) {
         if (err){
                 console.log(err);
@@ -34,7 +28,7 @@ router.route('/')
             }
             console.log(rows1);
             console.log(rows2);
-            res.render('mypage', { title: 'mypage', track1:rows1, track2:rows2 });
+            res.render('mypage', { title: 'mypage', track1:rows1[0], track2:rows2[0] });
         });
     });
 });
