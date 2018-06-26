433d0bbf424e08afba3d41cf2efd11482d503834
HOJONG_LAPTOP <jeontha@gmail.com>
2018-06-27 01:55:02 +0900

    Passed notices, fin_subjects data

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	console.log('session',req.session);
-	res.render('index', { title: 'Express', authID: req.session.student_id });
+router.route('/')
+.get((req, res, next) => {
+	var sql = 'SELECT * FROM Notice';
+	pool.query(sql, (err, notices_rows) => {
+		if (err)
+		{
+			console.log(err);
+		}
+		else
+		{
+			var sql = 'SELECT * FROM Fin_Subject WHERE ';
+			pool.query(sql, (err, fin_subject_rows) => {
+				if (err)
+				{
+					console.log(err);
+				}
+				else
+				{
+					res.render('index', {
+						title: 'index',
+						authID: req.session.student_id,
+						notices: notices_rows,
+						fin_subjects: fin_subject_rows,
+					});
+				}
+			})
+		}
+	});
 });
 
 module.exports = router;
