e88936055f631760ab582e5a4d205e2e139efee7
HOJONG_LAPTOP <jeontha@gmail.com>
2018-06-26 17:58:27 +0900

    Added student manage page

---
+++ routes/manage/student.js
@@ -0,0 +1,42 @@
+let util = require('util');
+let express = require('express');
+let router = express.Router();
+let app = express();
+let TABLE = 'Student';
+
+let error_handler = (error) => {
+	console.log('error:', error);
+};
+let success_handler = (result) => {
+	console.log('result:', result);
+};
+
+router.route('/')
+.get((req, resp, next) => {
+	let sql = util.format('SELECT * FROM %s', TABLE);
+	pool.query(sql, (error, rows) => {
+		if (error)
+			error_handler(error);
+		else
+		{
+			success_handler(rows);
+			resp.render('manage_student', {title: 'Manage Students', rows: rows});
+		}
+	});
+
+	
+})
+.post((req, resp, next) => {
+	let sql = util.format('insert into %s values(%s, %s, %s)', 
+		TABLE, req.body.id, req.body.name, req.body.pw);
+
+	pool.query(sql, (error, result) => {
+		if (error)
+			error_handler(error);
+		else
+			success_handler(result);
+		resp.redirect('student');
+	});
+});
+
+module.exports = router;
